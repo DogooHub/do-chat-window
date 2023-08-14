1b7930fdaca154ae4e5821c959838b36c105d35c
longnv <longnv@dogoo.vn>
2023-08-14 16:25:52 +0700

    update tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      screens: {
+        "dg__screen--1280": {"max": "1280px"}
+      }
+    },
+  },
+  plugins: [],
+  corePlugins: {
+    preflight: false
+  }
+}
+
